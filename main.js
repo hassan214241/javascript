b9a03437f676b6ebb0af36610aed0e5b3637b633
Muhammad Hassan <75841553+hassan214241@users.noreply.github.com>
2020-12-24 16:49:45 +0500

    js code

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+const marks = 20;
+
+if (marks >= 80){
+    console.log("Grade is A");
+}else if (marks >=70){
+    console.log("Grade is B");
+}else if (marks >=60){
+    console.log("Grade is c");
+}else {
+    console.log("failed");
+}
