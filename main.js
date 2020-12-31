fc1cb0f34ac0bc7c961f453d3dc3fa227d5a1815
Muhammad Hassan <75841553+hassan214241@users.noreply.github.com>
2020-12-31 21:10:48 +0500

    convert Temperature celsius to farhenheit

--- main.js
+++ main.js
@@ -88,3 +88,18 @@ function add(numberOne = 0, numberTwo = 0) {
     console.log(add(3));
     console.log(add());
 
+
+
+
+
+// convert Temperature celsius to farhenheit
+function getTemperature(c){
+    const result = 9/5 * c + 32 ;
+    console.log("convert temp celsius to fahrenheit: " +  c + " is " +  result);
+    return result;
+}
+
+getTemperature(96);
+getTemperature(24);
+getTemperature(34);
+
